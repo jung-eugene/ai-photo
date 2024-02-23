b34e97728dde353cc63b629b6a7f4e985b2b5b6f
jung <eugenejung04@gmail.com>
2024-02-23 03:32:04 -0500

    fix configuration error

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,18 +1,18 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import OpenAI from 'openai';
-import { Configuration, OpenAIApi } from 'openai';
+// import { Configuration, OpenAIApi } from 'openai';
 
 
 dotenv.config();
 
 const router = express.Router();
 
-const configuration = new Configuration({
+const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 })
 
-const openai = new OpenAIApi(configuration);
+// const openai = new OpenAIApi(configuration);
 
 router.route('/').get((req, res) => {
     res.send('Hello from DALL-E!');
