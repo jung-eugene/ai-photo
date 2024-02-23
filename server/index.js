cd622c2b313d363e167e68a6d3f26ad2637610ad
jung <eugenejung04@gmail.com>
2024-02-23 03:13:26 -0500

    initialize routes

--- server/index.js
+++ server/index.js
@@ -4,11 +4,18 @@ import cors from 'cors';
 
 import connectDB from './mongodb/connect.js';
 
+import postRoutes from './routes/postRoutes.js'
+import dalleRoutes from './routes/dalleRoutes.js'
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
+
+// create api endpoints to connect to frontend
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalle);
 
 app.get('/', async (req, res) => {
     res.send('Hello World!');
