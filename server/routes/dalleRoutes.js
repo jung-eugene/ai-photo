6a54a0ee580b3b7c2b5f18c8ab22d407cfca6a3c
jung <eugenejung04@gmail.com>
2024-02-23 04:00:07 -0500

    fix errors

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -23,20 +23,20 @@ router.route('/').post(async (req, res) => {
         const { prompt } = req.body;
 
         // get ai generated image
-        const aiResponse = await openai.createImage({
+        const aiResponse = await openai.images.generate({
             prompt,
             n: 1,
             size: '1024x1024',
             response_format: 'b64_json',
         });
 
-        const image = aiResponse.data.data[0].b64_json;
+        const image = aiResponse.data[0].b64_json;
 
         // send back to frontend
         res.status(200).json({ photo: image });
     } catch (error) {
         console.log(error);
-        res.status(500).send(error?.response.data.error.message)
+        res.status(500).send(error?.response.data.error.message);
     }
 })
 
