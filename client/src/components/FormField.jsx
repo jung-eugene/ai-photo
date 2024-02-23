c6b78f4f52e4d6f835c5abbb523d9f462b989e33
jung <eugenejung04@gmail.com>
2024-02-23 00:22:34 -0500

    field component

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -22,7 +22,7 @@ const FormField = ({
         <button
           type="button"
           onClick={handleSurpriseMe}
-          className="font-semibold text-xs bg-[#EcECF1] py-1 px-2 rounded-[5px] text-black"
+          className="font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black"
         >
           Surprise me
         </button>
@@ -32,11 +32,11 @@ const FormField = ({
       type={type}
       id={name}
       name={name}
-      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#6469ff] focus:border-[#6469ff] outline-none block w-full p-3"
       placeholder={placeholder}
       value={value}
       onChange={handleChange}
       required
+      className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#6469ff] focus:border-[#6469ff] outline-none block w-full p-3'
     />
   </div>
 );
