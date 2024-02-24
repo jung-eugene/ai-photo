946dbbe5ade87d8ae496cb760df3558acd3b08de
jung <eugenejung04@gmail.com>
2024-02-24 01:03:37 -0500

    show all posts

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -14,10 +14,38 @@ const RenderCards = ({ data, title }) => {
   };
 
 const Home = () => {
-    const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
   const [searchText, setsearchText] = useState('');
   
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/post', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+
+        if (response.ok) {
+          const result = await response.json();
+
+          // show newest post at the top
+          setAllPosts(result.data.reverse());
+        }
+      } catch (error) {
+        alert(error)
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchPosts();
+  }, []);
+
   return (
     <section className="max-w-7xl mx-auto">
       <div>
@@ -49,8 +77,8 @@ const Home = () => {
                     />
                 ) : (
                     <RenderCards
-                        data={[]}
-                        title="No Posts Yet"
+                        data={allPosts}
+                        title="No Posts Found"
                     />
                 )}
                 </div>
